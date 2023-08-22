97fcfba7aa73368a6df81d2f70079596b74da2c5
서보민 <sbmsky@gmail.com>
2023-08-22 22:17:50 +0900

    230822 added thread1_win

---
+++ 03_WindowProgramming/19_Thread/thread1_win.c
@@ -0,0 +1,27 @@
+#include <process.h>
+#include <stdio.h>
+#include <windows.h>
+unsigned WINAPI ThreadFunc(void *arg);
+
+int main(int argc, char *argv[]) {
+    HANDLE hThread;
+    unsigned threadID;
+    int param = 5;
+    hThread = (HANDLE)_beginthradex(NULL, 0, ThreadFunc, (void *)&param, 0, &threadID);
+    if (hThread == 0) {
+        puts("beginthreadex error");
+        return -1;
+    }
+    Sleep(3000);
+    puts("end of Main");
+    return 0;
+}
+
+unsigned WINAPI ThreadFunc(void *arg) {
+    int cnt = *((int *)arg);
+    for (int i = 0; i < cnt; i++) {
+        Sleep(1000);
+        puts("running thread");
+    }
+    return 0;
+}
